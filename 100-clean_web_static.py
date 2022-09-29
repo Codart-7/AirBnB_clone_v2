b3fdd13f5132b8eb660782ec6e5b59f81855424f
Codart-7 <iconmoa@gmail.com>
2022-09-30 00:32:24 +0100

    deploy Q3 & advanced

---
+++ 100-clean_web_static.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+""" Function that deploys """
+from fabric.api import *
+
+
+env.hosts = ['3.233.232.127', '44.200.43.251']
+env.user = "ubuntu"
+
+
+def do_clean(number=0):
+    """ CLEANS """
+
+    number = int(number)
+
+    if number == 0:
+        number = 2
+    else:
+        number += 1
+
+    local('cd versions ; ls -t | tail -n +{} | xargs rm -rf'.format(number))
+    path = '/data/web_static/releases'
+    run('cd {} ; ls -t | tail -n +{} | xargs rm -rf'.format(path, number))
